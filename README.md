fc1a93b49cd3eba5953800e7d1950f6e4816e369
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2022-12-15 13:29:09 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# Scraping website Using C/C++
+
+# Windows :
+
+- 1. Install MinGW
+- 2. Install CMake
+- 3. Install Git
+- 4. Install Visual Studio Code
+- 5. Install C/C++ Extension
+- 6. Install CMake Tools Extension
+
+<b> Download The Repository First </b>
+
+# Linux :
+
+- 1. Download the repository
+- 2. <h3> run file run.sh </h3>
+- 3. Install visual studio code / Nano / Vim
+    
+    ```./run.sh```
+    <br />
+    or
+    <br />
+    ``` bash run.sh ```
+
+<br />
+<br />
+<h3> Compile : </h3>
+
+```g++ main.cc -lcurl -lgumbo -std=c++17 -o main ```
+<br />
+<br />
+<h3> Run : </h3>
+
+```./main devil ```
+<br />
+<br />
+
+# Image
+
+<img src="./image/1.png" alt="Example output" width="100%" height="100%"/>
---
+++ run.sh
@@ -0,0 +1,69 @@
+# are you using vcpkg or debian
+echo "===================================="
+echo "Welcome to the C++ Web Scraper"
+echo "===================================="
+echo "Are you using vcpkg or debian? (v/u)"
+read jawab
+if [ $jawab = "v"]
+then
+    vcpkg install curl
+    vcpkg install gumbo
+    vcpkg integrate install
+    echo "Done..."
+else
+    echo "Installing Requirements..."
+    sudo apt install libcurl4-openssl-dev libgumbo-dev
+    echo "Done..."
+fi
+
+#mv ./curl2 ./curl
+#mv ./gumbo-parser2 ./gumbo-parser
+
+# curl install
+echo "===================================="
+cd ./curl
+autoreconf -fi
+./configure
+make
+# loading 3 seconds
+for i in {1..3}
+do
+    echo "Loading..."
+    sleep 1
+done
+echo "done..."
+
+# gumbo install
+echo "===================================="
+cd ../gumbo-parser
+./autogen.sh
+./configure
+make && make install
+
+echo "Do You have a C++ compiler installed? (y/n)"
+read answer
+if [ $answer = "y" ]
+then
+    echo "Compiling..."
+    g++ main.cc -lcurl -lgumbo -std=c++17 -o main
+    echo "Running..."
+    ./main doom
+else
+    echo "Please install a C++ compiler"
+    echo "Do You Want to install it ? (y/n)"
+    read answer
+    if [ $answer = "y" ]
+    then
+        echo "Installing..."
+        sudo apt install g++
+        echo "Compiling..."
+        g++ main.cc -lcurl -lgumbo -std=c++17 -o main
+        echo "Running..."
+        ./main doom
+    else
+        echo "Exiting..."
+    fi
+fi
+
+# g++ main.cc -lcurl -lgumbo -std=c++17 -o main
+# ./main doom
